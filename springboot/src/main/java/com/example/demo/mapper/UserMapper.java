53ba4be61bd3fb820c367621b07d48bfb694f7ea
huang-hanson <57970696+huang-hanson@users.noreply.github.com>
2021-07-15 20:51:06 +0800

    2021/7/15

---
+++ springboot/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.User;
+
+/**
+ * @author 黄忠
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
